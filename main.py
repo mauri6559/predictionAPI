7d073e023c130644db02c59baaaa6d427f828bc9
mauri6559 <git-intern@econotec.com.bo>
2024-11-23 00:20:10 -0400

    uni

--- main.py
+++ main.py
@@ -141,6 +141,6 @@ def predict_sales():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run()
 # if __name__ == '__main__':
 #     app.run(host="10.0.11.83", port=5000)
